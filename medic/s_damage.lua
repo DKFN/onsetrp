cf502386366ac60c61964160defe84b3293c8f38
Soljian / Sheaven <maxverr@gmail.com>
2020-01-28 23:39:25 +0100

    d

--- medic/s_damage.lua
+++ medic/s_damage.lua
@@ -16,7 +16,22 @@ end)
 
 AddEvent("OnPlayerWeaponShot", function(player, weapon, hittype, hitid, hitX, hitY, hitZ, startX, startY, normalX, normalY, normalZ)
     print('SHOT')
+
+    if hittype == 2 then -- player
+    else -- npc
+
+        local npcx,npcy,npcz = GetNPCLocation(hitid)
+        local npcFeetPos = npcz-90
+
+
+    end
 end)
+
+local npctest
+AddEvent("OnPackageStart", function()
+    npctest = CreateNPC(210165, 160910, 1305, 0)    
+end)
+
 
 
 AddEvent("OnPlayerDamage", function(player, damagetype, amount)
@@ -70,80 +85,80 @@ end)
 
 
 -- EN TEST
-local OGK_TRUE_DMG_DEBUG = true
-local debug_npc
-local HEADSHOT_BONUS = 35
-local CORPSE_BONUS = 10
+-- local OGK_TRUE_DMG_DEBUG = true
+-- local debug_npc
+-- local HEADSHOT_BONUS = 35
+-- local CORPSE_BONUS = 10
 
-local function debug(message)
-    if OGK_TRUE_DMG_DEBUG then
-        print("[OGK][GG] True DMG -- " .. message)
-    end
-end
+-- local function debug(message)
+--     if OGK_TRUE_DMG_DEBUG then
+--         print("[OGK][GG] True DMG -- " .. message)
+--     end
+-- end
 
-local function OnPlayerWeaponShot(player, weapon, hittype, hitid, hitx, hity, hitz, startx, starty, startz, normalx, normaly, normalz)
-    local healthSetter
-    local healthGetter
-    local positionGetter
+-- local function OnPlayerWeaponShot(player, weapon, hittype, hitid, hitx, hity, hitz, startx, starty, startz, normalx, normaly, normalz)
+--     local healthSetter
+--     local healthGetter
+--     local positionGetter
     
-    if hittype == 2 then
-        healthSetter = SetPlayerHealth
-        healthGetter = GetPlayerHealth
-        positionGetter = GetPlayerLocation
-        armorGetter = GetPlayerArmor
-    elseif hittype == 4 then
-        healthSetter = SetNPCHealth
-        healthGetter = GetNPCHealth
-        positionGetter = GetNPCLocation
-        armorGetter = function() return 0 end
-    end
+--     if hittype == 2 then
+--         healthSetter = SetPlayerHealth
+--         healthGetter = GetPlayerHealth
+--         positionGetter = GetPlayerLocation
+--         armorGetter = GetPlayerArmor
+--     elseif hittype == 4 then
+--         healthSetter = SetNPCHealth
+--         healthGetter = GetNPCHealth
+--         positionGetter = GetNPCLocation
+--         armorGetter = function() return 0 end
+--     end
     
-    if hittype == 2 or hittype == 4 then
-        -- First find the player that is in range of the hit
-        local victim = hitid
-        local victimx, victimy, victimz = positionGetter(hitid)
+--     if hittype == 2 or hittype == 4 then
+--         -- First find the player that is in range of the hit
+--         local victim = hitid
+--         local victimx, victimy, victimz = positionGetter(hitid)
         
         
-        local victim_feet_pos = victimz - 90;
+--         local victim_feet_pos = victimz - 90;
         
-        -- Finding where the hit happend and then adding bonus/malus to dmg
-        local hit_pos
-        local victim_health = healthGetter(hitid)
-        local final_health = victim_health
-        local crouched = GetPlayerMovementMode(hitid) == 4 -- 2 Walking 4 Crouched
-        local corpseThreshold
+--         -- Finding where the hit happend and then adding bonus/malus to dmg
+--         local hit_pos
+--         local victim_health = healthGetter(hitid)
+--         local final_health = victim_health
+--         local crouched = GetPlayerMovementMode(hitid) == 4 -- 2 Walking 4 Crouched
+--         local corpseThreshold
         
-        if crouched then
-            corpseThreshold = 25
-            headTreshold = 50
-        else
-            corpseThreshold = 50
-            headTreshold = 150
-        end
+--         if crouched then
+--             corpseThreshold = 25
+--             headTreshold = 50
+--         else
+--             corpseThreshold = 50
+--             headTreshold = 150
+--         end
         
-        -- TODO: Handle armor
-        if hitz > victim_feet_pos + corpseThreshold then
-            final_health = victim_health - CORPSE_BONUS
-        end
-        if hitz > victim_feet_pos + headTreshold then
-            CallRemoteEvent(player, "TrueDmgHeadShot")
-            final_health = victim_health - HEADSHOT_BONUS
-        end
+--         -- TODO: Handle armor
+--         if hitz > victim_feet_pos + corpseThreshold then
+--             final_health = victim_health - CORPSE_BONUS
+--         end
+--         if hitz > victim_feet_pos + headTreshold then
+--             CallRemoteEvent(player, "TrueDmgHeadShot")
+--             final_health = victim_health - HEADSHOT_BONUS
+--         end
         
-        if final_health <= 0 then
-            final_health = 1
-        end
+--         if final_health <= 0 then
+--             final_health = 1
+--         end
         
-        healthSetter(victim, final_health)
-        --debug("Registered hit by "..player.." | VICTIM FEETS "..victim_feet_pos.." | HIT Y: "..hity.." HIT Z: "..hitz)
-        debug("Pre-hit damage setting" .. final_health)
-    end
-end
-AddEvent("OnPlayerWeaponShot", OnPlayerWeaponShot)
+--         healthSetter(victim, final_health)
+--         --debug("Registered hit by "..player.." | VICTIM FEETS "..victim_feet_pos.." | HIT Y: "..hity.." HIT Z: "..hitz)
+--         debug("Pre-hit damage setting" .. final_health)
+--     end
+-- end
+-- AddEvent("OnPlayerWeaponShot", OnPlayerWeaponShot)
 
-if OGK_TRUE_DMG_DEBUG then
-    AddEvent("OnPackageStart", function()
-        debug("Debug mode")
-        debug_npc = CreateNPC(42180.0, 201287.0, 551.0, 0)
-    end)
-end
+-- if OGK_TRUE_DMG_DEBUG then
+--     AddEvent("OnPackageStart", function()
+--         debug("Debug mode")
+--         debug_npc = CreateNPC(42180.0, 201287.0, 551.0, 0)
+--     end)
+-- end
